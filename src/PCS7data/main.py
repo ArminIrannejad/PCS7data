44da0f64f5ecd5717bff5ca3149249f86e600f48
ArminIrannejad <armin.irannejad@gmail.com>
2024-07-01 14:41:19 +0200

    refactor: fetcher complete may need small refactoring

--- src/PCS7data/main.py
+++ src/PCS7data/main.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import os
 import re
+import pandas as pd
 from lxml import etree
 from concurrent.futures import ThreadPoolExecutor
 
@@ -8,22 +8,24 @@ class DataFetcher:
     def __init__(self):
         self.path = os.getenv("MY_PATH")
 
-    def fetcher(self):
-        pass    
-
-class DataProcessor:
-    def __init__(self):
-        self.path = os.getenv("MY_PATH")
-        
-    def file_filter(self, file, start_number, end_number, includes):
+    def file_filter(self, file, start_number, end_number, includes, excludes):
         m_pattern = re.compile(r'M(\d{3})')
         match = m_pattern.search(file)
         if match:
             number = int(match.group(1))
             if start_number <= number <= end_number:
-                if any(include in file for include in includes):
-                    return True
-        return False 
+                if any(include.lower() in file.lower() for include in includes) and not any(exclude.lower() in file.lower() for exclude in excludes):
+                    return file 
+        return None 
+
+    def fetcher(self, filenames, start_number, end_number, includes, excludes):
+        with ThreadPoolExecutor() as executor:
+            filtered_files = list(executor.map(lambda file: self.file_filter(file, start_number, end_number, includes, excludes), filenames))
+        return list(filter(None, filtered_files))
+
+class DataProcessor:
+    def __init__(self):
+        self.path = os.getenv("MY_PATH")
 
     def extract_batch_number(self, filename):
         ignore = 'SB8_34633-5543-83_'
@@ -33,6 +35,7 @@ class DataProcessor:
         if start_index != -1:
             result = filename[start_index: start_index + 10]
             return result
+        return None
 
     def process(self, file, xpath, namespace):
         results = [file]
@@ -48,17 +51,19 @@ class DataProcessor:
                 results.append(None)
         return results
 
-
-                
-
-
 def main():
     processor = DataProcessor()
-    filenames = processor.file_filter()
+    fetcher = DataFetcher()
+    path = fetcher.path
+    filenames = os.listdir(path)
 
-    with ThreadPoolExecutor() as executor:
-        batch_numbers = list(executor.map(processor.extract_batch_number, filenames))
-    print(batch_numbers)
+    start_number = 234
+    end_number = 460
+    includes = ["CIP", "516"]
+    excludes = ["TEST", "AL", "EXTRA", "BUFF", "SAT", "SIP",]
+
+    filtered_files = fetcher.fetcher(filenames, start_number, end_number, includes, excludes)
+    print(len(filtered_files))
 
 if __name__ == "__main__":
     main()
