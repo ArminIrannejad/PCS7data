b8b1fe838ab4cbbe4dbc0c25a6d22d1ec4f5591c
ArminIrannejad <armin.irannejad@gmail.com>
2024-07-04 13:13:35 +0200

    refactor: windows friendly

--- src/PCS7data/main.py
+++ src/PCS7data/main.py
@@ -39,7 +39,7 @@ class DataProcessor:
 
     def process(self, file, xpaths, namespaces):
         results = [file]
-        file_path = f'{self.path}/{file}'
+        file_path = fr'{self.path}\{file}'
         tree = etree.parse(file_path)
         root = tree.getroot()
         for xpath in xpaths.values():
@@ -56,7 +56,7 @@ class DataProcessor:
         xpath = "//ns:Archivebatch/ns:Cr"
         if '"' in file:
             return None
-        file_path = f'{self.path}/{file}'
+        file_path = fr'{self.path}\{file}'
         tree = etree.parse(file_path)
         root = tree.getroot()
         parvalfloats = root.xpath(xpath, namespaces=namespaces)
@@ -87,7 +87,6 @@ class DataProcessor:
             timestamps = list(executor.map(self.get_time_, files))
         start_times = [timestamp[0] if timestamp is not None else None for timestamp in timestamps]
         end_times = [timestamp[1] if timestamp is not None else None for timestamp in timestamps]
-        
         dict_time = {
                 'Batch_Numbers': batch_numbers,
                 'Start_time': start_times,
@@ -106,19 +105,43 @@ def main():
     fetcher = DataFetcher(path)
     filenames = os.listdir(path)
 
-    start_number = 102
+    start_number = 400
     end_number = 500 
-    includes = ["654"]
-    excludes = ["TEST", "EXTRA", "BUFF", "SAT", "SIP", "516", "MIN", "CIP", "656",]
+    includes = ["516"]
+    excludes = ["TEST", "EXTRA", "BUFF", "SAT", "SIP", "654", "MIN", "CIP", "656",]
 
     filtered_files = fetcher.fetcher(filenames, start_number, end_number, includes, excludes)
-    print(len(filtered_files))
 
     with ThreadPoolExecutor() as executor:
         batch_numbers = list(executor.map(processor.extract_batch_number, filtered_files))
-    print(len(batch_numbers))
 
     times = processor.get_time(batch_numbers, filtered_files)
-    print(times)
+    
+    namespaces = {'ns': 'SIMATIC_BATCH_V8_1_0'}
+    xpaths = {
+    "A_vikt": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='61' and @termid='5']/ns:Parvalcltn/ns:Parvalmaterial[@id='10' and @actval > '0']/@actval",
+    "IIISusp_V_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='5' and @termid='48']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "IIISusp_V_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='5' and @termid='31']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "VIExtr_V_add_IIISusp_Efter": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='8' and @termid='1']/ns:Parvalcltn/ns:Parvalfloat[@id='2' and @actval > '2']/@actval",
+    "VIExtr_V_efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='0' and @termid='1']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "VIIIFallning_V_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='1' and @termid='43']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "VIIIFallning_V_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='1' and @termid='45']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "XIVFallning_V_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='30' and @termid='13']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "XIVFallning_V_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='30' and @termid='18']/ns:Parvalcltn/ns:Parvalfloat[@id='22' and @actval > '2']/@actval",
+    "IIISusp_pH_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='5' and @termid='9']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0']/@actval",
+    "IIISusp_pH_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='5' and @termid='29']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0']/@actval",
+    "VIExtr_pH_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='0' and @termid='153']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0']/@actval",
+    "VIExtr_pH_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='0' and @termid='79']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0']/@actval",
+    "VIIIFallning_pH_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='1' and @termid='39']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0']/@actval",
+    "VIIIFallning_pH_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='1' and @termid='6']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '0' and @actval < '7']/@actval",
+    "XIVFallning_pH_Innan_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='30' and @termid='95']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '7']/@actval",
+    "XIVFallning_pH_Efter_pHjust": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='30' and @termid='25']/ns:Parvalcltn/ns:Parvalfloat[@id='14' and @actval > '7']/@actval"
+    }
+
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(lambda file: processor.process(file, xpaths, namespaces), filenames))
+
+    print(results)
+
 if __name__ == "__main__":
     main()
