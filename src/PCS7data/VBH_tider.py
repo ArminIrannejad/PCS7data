a5bc297d6391c7ea372c8b2de2127aad0c69ab9e
ArminIrannejad <armin.irannejad@gmail.com>
2024-08-25 18:40:47 +0200

    refactor: Added file check

--- src/PCS7data/VBH_tider.py
+++ src/PCS7data/VBH_tider.py
@@ -5,41 +5,59 @@ from data_fetcher import DataFetcher
 from data_processor import DataProcessor
 from datetime import datetime
 
+def process_time_data_in_chunks(files, processor, xpath_VBH, namespaces, chunk_size=10):
+    time_data = []
+    
+    for i in range(0, len(files), chunk_size):
+        chunk = files[i:i + chunk_size]
+        
+        with ThreadPoolExecutor() as executor:
+            result_time = list(executor.map(lambda file: processor.process(file, xpath_VBH, namespaces, 'full'), chunk))
+        
+        chunk_time_data = [[processor.time_difference(timestamps) if timestamps is not None else None for timestamps in row] for row in result_time]
+        time_data.extend(chunk_time_data)
+    
+    return time_data
+
 def main():
-    path = os.getenv("MY_PATH")
-    path = '/mnt/c/Users/se1irar/Downloads/Archive/'
+    path = "C:/Users/se1irar/Downloads/Archive/"
+    path = "Z:/Production/ALBFRII/BatchArchives_XML/"
+    output_path = "C:/Users/se1irar/Documents/Data/Albumin/"
+    output_path1 = "Z:/Public/Temp/se1irar/Data/Albumin/"
+
     processor = DataProcessor(path)
     fetcher = DataFetcher(path)
     filenames = os.listdir(path)
 
-    start_number = 351
-    end_number = 351
+    start_number = 400
+    end_number = 450
     includes = ["656"]
     includes2 = ["657"]
-    excludes = ["TEST", "EXTRA", "BUFF", "SAT", "SIP", "516", "MIN", "CIP", "654", "GRF", "GRT", "ALF", "ALT", ]
+    excludes = ["TEST", "EXTRA", "BUFF", "SAT", "SIP", "516", "MIN", "CIP", "654", "GRF", "GRT", "ALF", "ALT"]
 
     filtered_files_656 = fetcher.fetcher(filenames, start_number, end_number, includes, excludes)
     filtered_files_657 = fetcher.fetcher(filenames, start_number, end_number, includes2, excludes)
 
-    with ThreadPoolExecutor() as executor:
-        batch_numbers_656 = list(executor.map(processor.extract_batch_number, filtered_files_656))
-
-    with ThreadPoolExecutor() as executor:
-        batch_numbers_657 = list(executor.map(processor.extract_batch_number, filtered_files_657))
-
     filtered_files_656 += filtered_files_657
-    batch_numbers_656 += batch_numbers_657
-    print(len(batch_numbers_656))
+
+    if os.path.exists(output_path):
+        existing_df = pd.read_csv(output_path)
+        existing_filenames = set(existing_df['Filename'].tolist())
+    else:
+        existing_filenames = set()
+
+    new_files = [file for file in filtered_files_656 if file not in existing_filenames]
+
+    batch_numbers_656 = list(map(processor.extract_batch_number, new_files))
 
     namespaces = {'ns': 'SIMATIC_BATCH_V8_1_0'}
     xpath_VBH = {
             "VBH": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='31' and @termid='4']/@timestamp",
     }
 
-    with ThreadPoolExecutor() as executor:
-        result_time = list(executor.map(lambda file: processor.process(file, xpath_VBH, namespaces, 'full'), filtered_files_656))
-    
-    time_data = [[processor.time_difference(timestamps) if timestamps is not None else None for timestamps in row] for row in result_time]
+    chunk_size = 20  
+    time_data = process_time_data_in_chunks(new_files, processor, xpath_VBH, namespaces, chunk_size)
+
     difference = [timestamps[0] if timestamps is not None else None for row in time_data for timestamps in row]
     start_times = [timestamps[1].strftime("%Y-%m-%dT%H:%M:%S.%fZ") if timestamps is not None else None for row in time_data for timestamps in row]
     end_times = [timestamps[2].strftime("%Y-%m-%dT%H:%M:%S.%fZ") if timestamps is not None else None for row in time_data for timestamps in row]
@@ -54,6 +72,12 @@ def main():
 
     df = df.dropna()
     print(df)
+    if os.path.exists(output_path):
+        df.to_csv(output_path, mode='a', header=False, index=False)
+        df.to_csv(output_path1, mode='a', header=False, index=False)
+    else:
+        df.to_csv(output_path, index=False)
+        df.to_csv(output_path1, index=False)
 
 if __name__ == "__main__":
     main()
