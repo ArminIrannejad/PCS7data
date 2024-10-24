31b3d2c39288b17aff83da91d28e464492bdec06
ArminIrannejad <armin.irannejad@gmail.com>
2024-10-24 09:57:31 +0200

    refactor: checkpoint, cba branch

--- src/PCS7data/CIP.py
+++ src/PCS7data/CIP.py
@@ -15,7 +15,7 @@ def process_chunks(files, processor, xpaths, namespaces, chunk_size=10):
 
 def main():
     if os.name == 'nt':
-        path = "C:/Users/se1irar/Downloads/Archive/"
+        path = "C:/Users/se1irar/Downloads/Archive/"  
         path = "Z:/Production/ALBFRII/BatchArchives_XML/"
     else:
         path = os.getenv("MY_PATH")
@@ -40,12 +40,12 @@ def main():
         "data": "/ns:Archivebatch/ns:Cr/ns:Rp/ns:Sfcdesccltn/ns:Sfcdescflat/ns:Sfcand",
     }
 
-    chunk_size = 10
+    chunk_size = 30
     result = process_chunks(filtered_files, processor, xpath_batch, namespaces, chunk_size)
     columns_grfp = ['Filename', 'CIPSats_Kallvatten', 'CIP_Dranering', 'CIPSats_NaOH', 'CIP_Dranering2', 'CIPSats_Kallvatten2', 'CIP_Dranering3', 'CIPSats_HWFI', 'CIP_Blasning']
     columns = ['Filename', 'CIPSats_Kallvatten', 'CIP_Dranering', 'CIPSats_NaOH', 'CIP_Dranering2', 'CIPSats_HWFI', 'CIP_Dranering3', 'CIP_Blasning', 'HygSta']
     
-    df = pd.DataFrame(result, columns=columns)
+    df = pd.DataFrame(result, columns=columns) # pyright: ignore
     df_grt11 = df[df['Filename'].str.contains('GRT11', case=False)]
     df_grt12 = df[df['Filename'].str.contains('GRT12', case=False)]
     df_grt13 = df[df['Filename'].str.contains('GRT13', case=False)]
@@ -66,6 +66,7 @@ def main():
         df.columns = columns_grfp
 
     print(df_grfp1_grt15)
+
     xpath_grfp = {
             "CIPSats_Kallvatten": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='0' and @termid='40']/@timestamp",
             }
