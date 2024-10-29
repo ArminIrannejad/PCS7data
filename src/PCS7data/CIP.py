bcf488ad4e8af91d3125beeda63aea0bc2a9b1ad
ArminIrannejad <armin.irannejad@gmail.com>
2024-10-29 12:34:05 +0100

    refactor: checkpoint, cba branch

--- src/PCS7data/CIP.py
+++ src/PCS7data/CIP.py
@@ -5,6 +5,7 @@ from concurrent.futures import ThreadPoolExecutor
 from data_fetcher import DataFetcher
 from data_processor import DataProcessor
 
+
 def process_chunks(files, processor, xpaths, namespaces, chunk_size=10):
     all_sfcands_data = []
     for i in range(0, len(files), chunk_size):
@@ -26,7 +27,7 @@ def main():
     filenames = os.listdir(path)
 
     start_number = 400
-    end_number = 407
+    end_number = 402
     includes = ["CIP", "516",]
     excludes = ["TEST", "EXTRA", "BUFF", "SAT", "SIP", "MIN", "654", "ALF", "ALT", "656"]
 
@@ -40,18 +41,18 @@ def main():
         "data": "/ns:Archivebatch/ns:Cr/ns:Rp/ns:Sfcdesccltn/ns:Sfcdescflat/ns:Sfcand",
     }
 
-    chunk_size = 30
+    chunk_size = 15
     result = process_chunks(filtered_files, processor, xpath_batch, namespaces, chunk_size)
     columns_grfp = ['Filename', 'CIPSats_Kallvatten', 'CIP_Dranering', 'CIPSats_NaOH', 'CIP_Dranering2', 'CIPSats_Kallvatten2', 'CIP_Dranering3', 'CIPSats_HWFI', 'CIP_Blasning']
     columns = ['Filename', 'CIPSats_Kallvatten', 'CIP_Dranering', 'CIPSats_NaOH', 'CIP_Dranering2', 'CIPSats_HWFI', 'CIP_Dranering3', 'CIP_Blasning', 'HygSta']
     
-    df = pd.DataFrame(result, columns=columns) # pyright: ignore
-    df_grt11 = df[df['Filename'].str.contains('GRT11', case=False)]
-    df_grt12 = df[df['Filename'].str.contains('GRT12', case=False)]
-    df_grt13 = df[df['Filename'].str.contains('GRT13', case=False)]
-    df_grt14 = df[df['Filename'].str.contains('GRT14', case=False)]
-    df_grt15 = df[df['Filename'].str.contains('GRT15', case=False)]
-    df_grt16 = df[df['Filename'].str.contains('GRT16', case=False)]
+    df = pd.DataFrame(result, columns=columns) # pyright: ignore should be fixed in 2.3.* pandas
+    df_grt11 = df[df['Filename'].str.contains('GRT11', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
+    df_grt12 = df[df['Filename'].str.contains('GRT12', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
+    df_grt13 = df[df['Filename'].str.contains('GRT13', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
+    df_grt14 = df[df['Filename'].str.contains('GRT14', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
+    df_grt15 = df[df['Filename'].str.contains('GRT15', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
+    df_grt16 = df[df['Filename'].str.contains('GRT15', case=False) & ~df['Filename'].str.contains('GRFP', case=False)]
     df_grfp1_grt11 = df[df['Filename'].str.contains('GRFP1', case=False) & df['Filename'].str.contains('GRT11', case=False)]
     df_grfp1_grt13 = df[df['Filename'].str.contains('GRFP1', case=False) & df['Filename'].str.contains('GRT13', case=False)]
     df_grfp1_grt15 = df[df['Filename'].str.contains('GRFP1', case=False) & df['Filename'].str.contains('GRT15', case=False)]
@@ -65,16 +66,21 @@ def main():
     for df in dfs_filter:
         df.columns = columns_grfp
 
-    print(df_grfp1_grt15)
+    print(df_grt14)
+    aoneuth = df_grt16, df_grt12, df_grt13, df_grt11, df_grt15, df_grt14
 
-    xpath_grfp = {
-            "CIPSats_Kallvatten": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='0' and @termid='40']/@timestamp",
-            }
-"""
-    xpath_VBH = {
-            "VBH": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='XX' and @termid='XX']/@timestamp",
-            }
-"""
+    tuples_list = []
+    for index, row in df_grt14.iterrows():
+        tuples_list.append(row['CIPSats_Kallvatten'])
+
+    result_dict_grt14_sfc1 = {t[0]: f"/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid={t[1]} and @termid={t[2]}]/@timestamp" for t in tuples_list[0]}
+    print(result_dict_grt14_sfc1)
+
 if __name__ == "__main__":
     main()
 
+    """
+    xpath_VBH = {
+            "VBH": "/ns:Archivebatch/ns:Cr/ns:Eventcltn/ns:Eventrph[@contid='XX' and @termid='XX']/@timestamp",
+            }
+    """
